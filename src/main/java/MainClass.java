cb8a8daa143f1e5ee8798e563ba60cefe1f5a6db
Hunaid Husain <hunaidee007@gmail.com>
2023-07-14 09:31:45 +0100

    Polymorphism
    Using the reference of super class and Object of Sub Class.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,18 +2,37 @@ public class MainClass {
 
     public static void main (String args [] ) throws Exception {
 
-     Account account = new Account("M", 100,1985);
+    /* Account account = new Account("M", 100,1985);
      Account account1 = new Account("A", 500,1990);
 
      System.out.println(account1.getAge());
 
-     System.out.println(account.getAge());
+     System.out.println(account.getAge());*/
 
      SavingsAccount savingsAccount = new SavingsAccount("A", 500,1990);
 
-     savingsAccount.withdraw(400);
+     //savingsAccount.withdraw(400);
+        int a = 5;
+        System.out.println(a);
+        doSomething(a);
+        System.out.println(a);
+
+        doSomethingWithAccount(savingsAccount);
+
+        System.out.println(savingsAccount.getBalance());
 
 
+    }
+
+
+    public static void doSomethingWithAccount(SavingsAccount tempAccount){ /// SavingsAccount tempAccount = savingsAccount;
+        tempAccount.balance=600;
+    }
+
+    public static void doSomething(int i) { //int i = a;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
     }
 
 }
